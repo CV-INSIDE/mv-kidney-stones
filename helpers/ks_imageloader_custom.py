fac0bdac24cce2eb06a2c9757604c7f03e99c2d9
EliasVillalvazo <eav.0609@gmail.com>
2022-06-29 01:27:52 -0500

    Create files for multi_view

---
+++ helpers/ks_imageloader_custom.py
@@ -0,0 +1,132 @@
+import os
+import random
+import logging
+import numpy as np
+import pytorch_lightning as pl
+
+from torchvision import datasets
+from torch.utils.data import DataLoader, SubsetRandomSampler
+
+from helpers.data_loader import ColorDataSetMV
+from helpers.data_separator import get_content_of_folder
+
+log = logging.getLogger(__name__)
+
+
+class KidneyImagesLoader(pl.LightningDataModule):
+    """
+    Class to pull the zip file from google drive and returns the data loaders
+    for the training, validation and tests sets. It receives as input the path of
+    the zip file, the percentage of the training set that is going to be used for validation
+    and the transformations to be applied to the train/val sets and the test set.
+    """
+
+    def __init__(self,
+                 zip_path=None,
+                 images_path=None,
+                 val_percentage=0.2,
+                 train_batch_size=1,
+                 train_transformations=[],
+                 test_transformations=[],
+                 seed=None):
+        super().__init__()
+        self.num_workers = 0
+        if seed != None:
+            random.seed(seed)
+            np.random.seed(seed)
+            pl.seed_everything(seed)
+
+        if not zip_path and not images_path:
+            raise Exception('Zip path or images path is required')
+
+        if zip_path:
+            self.zip_path = zip_path
+            self.image_path = self.zip_path[self.zip_path.rindex("/"):]
+            self.image_path = "/content" + self.image_path.replace(".zip", "")
+        else:
+            self.image_path = images_path
+
+        self.val_percentage = val_percentage
+        self.train_batch_size = train_batch_size
+        self.train_transformations = train_transformations
+        self.test_transformations = test_transformations
+
+    def get_class_indices(self):
+        """
+        Returns the classes found.
+        """
+        return self.idx2class
+
+    def get_train_dataset(self):
+        """
+        Returns the training dataset and the mapping for index to class name.
+        """
+        print("Using color transformation dataset")
+        log.info("Using color transformation dataset")
+        dataset = ColorDataSetMV(get_content_of_folder(os.path.join(self.image_path, 'train')),
+                                 transform=self.train_transformations)
+        log.info(f"length of dataset {len(dataset)}")
+        idx2class = {v: k for k, v in dataset.class_to_idx.items()}
+        self.idx2class = idx2class
+        self.train_dataset = dataset
+        return dataset, idx2class
+
+    def _train_val_samplers(self):
+        """
+        Splits the training set into train and validation sets and it returns the
+        generated SubsetRandomSamplers.
+        """
+        rps_dataset_size = len(self.train_dataset)
+        rps_dataset_indices = list(range(rps_dataset_size))
+        np.random.shuffle(rps_dataset_indices)
+
+        val_split_index = int(np.floor(self.val_percentage * rps_dataset_size))
+        train_idx, val_idx = rps_dataset_indices[val_split_index:], rps_dataset_indices[:val_split_index]
+
+        self.train_sampler = SubsetRandomSampler(train_idx)
+        self.val_sampler = SubsetRandomSampler(val_idx)
+
+        log.info(f"train batch size: {train_idx}, val batch size: {val_idx}")
+
+    def get_test_dataset(self):
+        """
+        Returns the test dataset.
+        """
+        log.info("Using color dataset for testing")
+        dataset_test = ColorDataSetMV(get_content_of_folder(os.path.join(self.image_path, 'test')),
+                                      transform=self.test_transformations)
+        return dataset_test
+
+    def setup(self, stage=None):
+        """
+        Executed automatically by lightning when a dataloader is requested.
+        """
+        if hasattr(self, 'zip_path'):
+            cmd = 'yes | cp "' + self.zip_path + '" -d "/content/"'
+            os.system(cmd)
+            cmd = 'unzip -o --qq ' + self.image_path + ".zip" + ' -d "' + self.image_path + '"'
+            os.system(cmd)
+        self.get_train_dataset()
+        self._train_val_samplers()
+
+    def train_dataloader(self):
+        """
+        Returns the train data loader.
+        """
+        return DataLoader(dataset=self.train_dataset, shuffle=False, batch_size=self.train_batch_size,
+                          sampler=self.train_sampler, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        """
+        Returns the validation data loader.
+        """
+        return DataLoader(dataset=self.train_dataset, shuffle=False, batch_size=1, sampler=self.val_sampler,
+                          num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        """
+        Returns the test data loader.
+        """
+        dataset = self.get_test_dataset()
+        print(f"length of dataset {len(dataset)}")
+        return DataLoader(dataset=dataset, shuffle=False, batch_size=1, num_workers=self.num_workers)
---
+++ models/alexnet_mv_color.py
@@ -0,0 +1,74 @@
+"""
+Class that contains multi-view model structure for alexnet using three different color channels
+"""
+import torch
+from einops import reduce
+
+import helpers.transfer_learning_basemodel_mv_custom as tlm
+import torchvision.models as models
+import torch.nn as nn
+
+class AlexnetMax(tlm.BaseModel):
+    def __init__(self, hparams={}, num_classes=6, batch_size=64, pretrained=False, seed=None):
+        if "lr" not in hparams:
+            hparams["lr"] = 0.001
+
+        #LOG INFO
+        hparams["num_classes"] = num_classes
+        hparams["batch_size"] = batch_size
+        hparams["is_pretrained"] = pretrained
+        super(AlexnetMax, self).__init__(hparams, seed=seed)
+
+        self.alex = models.alexnet(pretrained=pretrained)
+        # freeze fully connected layers
+        for param in self.alex.parameters():
+            param.requires_grad = False
+
+        # create the fully connected classifier
+        self.alex.classifier = nn.Sequential(nn.Dropout(p=0.5),
+                                             nn.Linear(9216, 4096),
+                                             nn.ReLU(),
+                                             nn.Dropout(p=0.5),
+                                             nn.Linear(4096, 256),
+                                             nn.ReLU(),
+                                             nn.Linear(256, num_classes))
+
+        # define batch size and loss function
+        self.batch_size = batch_size
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, x, y, z):
+        """
+        Perform the forward step of multi view model. Fusion is done by concatenation of the values from different
+        views. The first layer of the classifier will then be: number_views * samples
+        The way it works is at follows:
+            1) First, do a forward step by execution of features, avg pool and flatten. The result will be a tensor of
+            size 1, 9216
+            2) Concatenate the feature vectors of the different views and connect them to a fc layer
+            3) Perform the classification task with RELU and dropout of 50%
+            4) The last step consists on the connection to the fc last layer to a softmax activation (not here)
+        :param x: batch of the first view
+        :param y: batch of the second view
+        :param z: batch of the third view
+        :return: the features after forward propagation
+        """
+        x = self.alex.features(x)
+        x = self.alex.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        y = self.alex.features(y)
+        y = self.alex.avgpool(y)
+        y = torch.flatten(y, 1)
+
+        z = self.alex.features(z)
+        z = self.alex.avgpool(z)
+        z = torch.flatten(z, 1)
+
+        # xy = torch.cat((x, y), 0)
+        xyz = torch.stack([x, y, z])
+        xyz = reduce(xyz, 'c b l -> 1 b l', 'max')
+        xyz = torch.squeeze(xyz, dim=0)
+        # xy = reduce(xy, 'c l -> 1 l', 'max')
+        xyz = self.alex.classifier(xyz)
+
+        return xyz
